07c030f34b40977ace58c8cba7a4eec11e35acbc
gisus <theegiza@gmail.com>
2026-01-26 21:38:52 +0200

    "fix"

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(function (_a) {
     var command = _a.command;
     return ({
-        base: '/BBMAP/',
         plugins: [react()],
-        
+        base: './',
         server: {
             open: true,
             port: 5173,
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/BBMAP/' : '/',
+  base: './',
   server: {
     open: true,
     port: 5173,
